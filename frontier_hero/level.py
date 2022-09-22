bff9e8d94360a501f5ac780cca0d70669aa3d84b
Dan Munro <dan@danmunro.com>
2022-09-21 21:56:02 -0700

    clean up

--- frontier_hero/level.py
+++ frontier_hero/level.py
@@ -38,11 +38,6 @@ class Level:
         value = self.get_tile(x, y).get(name)
         return value in (True, 1, 'true', 'yes', 'True', 'Yes', '1', 'on', 'On')
 
-    def is_wall(self, x, y):
-        """Is there a wall?"""
-
-        return self.get_bool(x, y, 'wall')
-
     def is_blocking(self, x, y):
         """Is this place blocking movement?"""
 
@@ -51,50 +46,13 @@ class Level:
         return self.get_bool(x, y, 'block')
 
     def render(self):
-        wall = self.is_wall
         tiles = self.cache[self.tileset]
         image = Surface((self.width * self.tile_width, self.height * self.tile_height))
         overlays = {}
         for map_y, line in enumerate(self.background):
             for map_x, c in enumerate(line):
-                if wall(map_x, map_y):
-                    # Draw different tiles depending on neighbourhood
-                    if not wall(map_x, map_y + 1):
-                        if wall(map_x + 1, map_y) and wall(map_x - 1, map_y):
-                            tile = 1, 2
-                        elif wall(map_x + 1, map_y):
-                            tile = 0, 2
-                        elif wall(map_x - 1, map_y):
-                            tile = 2, 2
-                        else:
-                            tile = 3, 2
-                    else:
-                        if wall(map_x + 1, map_y + 1) and wall(map_x - 1, map_y + 1):
-                            tile = 1, 1
-                        elif wall(map_x + 1, map_y + 1):
-                            tile = 0, 1
-                        elif wall(map_x - 1, map_y + 1):
-                            tile = 2, 1
-                        else:
-                            tile = 3, 1
-                    # Add overlays if the wall may be obscuring something
-                    if not wall(map_x, map_y - 1):
-                        if wall(map_x + 1, map_y) and wall(map_x - 1, map_y):
-                            over = 1, 0
-                        elif wall(map_x + 1, map_y):
-                            over = 0, 0
-                        elif wall(map_x - 1, map_y):
-                            over = 2, 0
-                        else:
-                            over = 3, 0
-                        overlays[(map_x, map_y)] = tiles[over[0]][over[1]]
-                else:
-                    try:
-                        tile = self.key[c]['tile'].split(',')
-                        tile = int(tile[0]), int(tile[1])
-                    except (ValueError, KeyError):
-                        # Default to ground tile
-                        tile = 0, 3
+                tile = self.key[c]['tile'].split(',')
+                tile = int(tile[0]), int(tile[1])
                 tile_image = tiles[tile[0]][tile[1]]
                 image.blit(tile_image,
                            (map_x * self.tile_width, map_y * self.tile_height))
