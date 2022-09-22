714c7b0f9b720513da88952193c4e2d41faf7c4f
Dan Munro <dan@danmunro.com>
2022-09-21 21:53:29 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from frontier_hero.level import Level
+import pygame
+
+
+def load_tile_table(filename, width, height):
+    image = pygame.image.load(filename).convert()
+    image_width, image_height = image.get_size()
+    tile_table = []
+    for tile_x in range(0, round(image_width / width)):
+        line = []
+        tile_table.append(line)
+        for tile_y in range(0, round(image_height / height)):
+            rect = (tile_x * width, tile_y * height, width, height)
+            line.append(image.subsurface(rect))
+    return tile_table
+
+
+if __name__ == "__main__":
+    screen = pygame.display.set_mode((424, 320))
+
+    MAP_TILE_WIDTH = 16
+    MAP_TILE_HEIGHT = 16
+    MAP_CACHE = {
+        'basictiles.png': load_tile_table('resources/basictiles.png', MAP_TILE_WIDTH,
+                                          MAP_TILE_HEIGHT),
+    }
+
+    level = Level(MAP_CACHE, MAP_TILE_WIDTH, MAP_TILE_HEIGHT, 'resources/level.map')
+
+    clock = pygame.time.Clock()
+
+    background, overlay_dict = level.render()
+    overlays = pygame.sprite.RenderUpdates()
+    for (x, y), image in overlay_dict.items():
+        overlay = pygame.sprite.Sprite(overlays)
+        overlay.image = image
+        overlay.rect = image.get_rect().move(x * 24, y * 16 - 16)
+    screen.blit(background, (0, 0))
+    overlays.draw(screen)
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
