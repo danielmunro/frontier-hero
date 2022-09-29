15eb0b1849341b4a3be7ad6f22c5480c6c5555a4
Dan Munro <dan@danmunro.com>
2022-09-28 20:00:30 -0700

    name

--- frontier_hero/item.py
+++ frontier_hero/item.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 
 class ItemType(Enum):
-    MAGIC_MIRROR = 1
+    DELICATE_MIRROR = 1
 
 
 class Item:
@@ -16,10 +16,10 @@ def magic_mirror():
     return Item(
         "a beautifully delicate mirror",
         None,
-        ItemType.MAGIC_MIRROR,
+        ItemType.DELICATE_MIRROR,
     )
 
 
 def item_factory(item_type: int):
-    if item_type == ItemType.MAGIC_MIRROR.value:
+    if item_type == ItemType.DELICATE_MIRROR.value:
         return magic_mirror()
